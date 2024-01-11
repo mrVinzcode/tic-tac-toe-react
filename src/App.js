2ebe7c33bbf336d93262d9af1655d690efccfea2
mrVinzcode <137170998+mrVinzcode@users.noreply.github.com>
2024-01-12 02:37:14 +0800

    fix all bugs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import { useState, React } from 'react';
-
+import "./App.css";
+import { useState, React } from "react";
 
 function App() {
-  
-  const [ boardData, setBoardData ] = useState([null, null, null, null, null, null, null, null, null]);
-  const [ player, setPlayer ] = useState('X');
+  const [boardData, setBoardData] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [player, setPlayer] = useState("X");
 
   const updateBoard = (index) => {
     const newBoardData = [...boardData];
-    if(newBoardData[index] === null) {
+    if (newBoardData[index] === null) {
       newBoardData[index] = player;
       setBoardData(newBoardData);
       switchPlayer();
     }
-  }
+  };
 
   const switchPlayer = () => {
-    if (player === 'X') {
-      setPlayer('O');
+    if (player === "X") {
+      setPlayer("O");
     } else {
-      setPlayer('X');
+      setPlayer("X");
     }
-  }
+  };
 
   const checkForTie = () => {
     let isTie = true;
@@ -32,7 +40,7 @@ function App() {
       }
     });
     return isTie;
-  }
+  };
 
   const checkForWinner = () => {
     const winningCombos = [
@@ -43,47 +51,63 @@ function App() {
       [1, 4, 7],
       [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6]
+      [2, 4, 6],
     ];
 
-    if(checkForTie()) {
-      return 'Tie';
+    if (checkForTie()) {
+      return "Tie";
     }
     for (let i = 0; i < winningCombos.length; i++) {
       const [a, b, c] = winningCombos[i];
-      if (boardData[a] && boardData[a] === boardData[b] && boardData[a] === boardData[c]) {
-        console.log('winner:', boardData[a]);
-        return(boardData[a]);
+      if (
+        boardData[a] &&
+        boardData[a] === boardData[b] &&
+        boardData[a] === boardData[c]
+      ) {
+        console.log("winner:", boardData[a]);
+        return boardData[a];
       }
     }
-    return(null);
-  }
+    return null;
+  };
 
-  function Tile({value, index}){
-    return(
+  function Tile({ value, index }) {
+    return (
       <div className="Tile" onClick={() => updateBoard(index)}>
         <h1>{value}</h1>
       </div>
-    )
+    );
   }
-  
-  function Board(){
-    return(
+
+  function Board() {
+    return (
       <div className="Board">
-        {
-          boardData.map((tile, index) => {
-            return <Tile key={index} value={tile} index={index}></Tile>
-          })
-        }
+        {boardData.map((tile, index) => {
+          return <Tile key={index} value={tile} index={index}></Tile>;
+        })}
       </div>
-    )
+    );
   }
 
   return (
     <div className="App">
       <Board></Board>
-      {checkForWinner() ? checkForWinner()==='Tie' ? <h1>Tie game</h1> : <h1>Winner: {checkForWinner()}</h1> : null}
-      {checkForWinner() ? <button onClick={() => setBoardData([null, null, null, null, null, null, null, null, null])}>New Game</button> : null}
+      {checkForWinner() ? (
+        checkForWinner() === "Tie" ? (
+          <h1>Tie game</h1>
+        ) : (
+          <h1>Winner: {checkForWinner()}</h1>
+        )
+      ) : null}
+      {checkForWinner() ? (
+        <button
+          onClick={() =>
+            setBoardData([null, null, null, null, null, null, null, null, null])
+          }
+        >
+          New Game
+        </button>
+      ) : null}
     </div>
   );
 }
